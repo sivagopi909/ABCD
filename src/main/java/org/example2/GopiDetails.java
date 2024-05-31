063351958251d84ce242d69d51fb7061fb666e6e
sivasadam <sivagopi909@gmail.com>
2024-05-30 22:50:29 -0500

    creation of object in class

--- src/main/java/org/example2/GopiDetails.java
+++ src/main/java/org/example2/GopiDetails.java
@@ -3,9 +3,12 @@ package org.example2;
 public class GopiDetails {
     public String name = "gopi";
     public int height =10;
+    public float weight=88.5f;
+    public double salary = 7500.50d;
     public static void main(String[] args){
         System.out.println("gopidetails");
         GopiDetails gopiDetails = new GopiDetails();
         System.out.println(gopiDetails.height);
+        System.out.println(gopiDetails.weight);
     }
 }
