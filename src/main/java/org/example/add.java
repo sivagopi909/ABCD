8720627f348e409cf0ce6f0448002db20ee01e5f
sivasadam <sivagopi909@gmail.com>
2024-06-01 11:37:33 -0500

    db cong

--- src/main/java/org/example/add.java
+++ src/main/java/org/example/add.java
@@ -4,12 +4,18 @@ public class add {
     public static void main(String [] args){
   int a=5;
   int b=6;
+  char c = '@';
+  boolean d = true;
+  boolean e = false;
   int sum= add(a,b);
   System.out.println("The sum is :" +sum);
     }
     public static int add(int a, int b){
         add Add = new add();
         System.out.println(add.class);
+        System.out.println(boolean.class);
+        System.out.println(Add.getClass());
+
         return a+b;
     }
 
