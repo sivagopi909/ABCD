e75dbcfbc3591755d18b385d3abdde8b53307bbf
sivasadam <sivagopi909@gmail.com>
2024-06-03 12:42:35 -0500

    creation of variables ouside the method

--- src/main/java/org/example/add.java
+++ src/main/java/org/example/add.java
@@ -8,13 +8,12 @@ public class add {
   boolean d = true;
   boolean e = false;
   int sum= add(a,b);
+  System.out.println(c);
   System.out.println("The sum is :" +sum);
     }
     public static int add(int a, int b){
         add Add = new add();
         System.out.println(add.class);
-        System.out.println(boolean.class);
-        System.out.println(Add.getClass());
 
         return a+b;
     }
