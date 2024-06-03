12d1d76daa6c4cde9a6fd17d5d4a7e26993b6a14
sivasadam <sivagopi909@gmail.com>
2024-06-03 12:44:36 -0500

    creation of variables inside the method

--- src/main/java/org/example2/Jobless.java
+++ src/main/java/org/example2/Jobless.java
@@ -5,16 +5,18 @@ public class Jobless {
     private static Jobless jobless;
     int months = 12;
     int days = 365;
-    float hours = 8 * 365f;
+    float hours = 16 * 365f;
 
     public Jobless(int months, int days, float hours, String company) {
     }
 
     public static void main(String[] args) {
         Jobless jobless = new Jobless(12, 365, 8 * 365f, "Google");
+        String company = "Google";
         System.out.println(jobless.months);
         System.out.println(jobless.days);
         System.out.println(jobless.hours);
+        System.out.println(company);
     }
 }
 
