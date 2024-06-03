704f83bc71bf023e3b047f67681a49cc2552bf89
sivasadam <sivagopi909@gmail.com>
2024-06-03 12:42:45 -0500

    creation of variables ouside the method

---
+++ src/main/java/org/example2/Jobless.java
@@ -0,0 +1,20 @@
+package org.example2;
+
+public class Jobless {
+
+    private static Jobless jobless;
+    int months = 12;
+    int days = 365;
+    float hours = 8 * 365f;
+
+    public Jobless(int months, int days, float hours, String company) {
+    }
+
+    public static void main(String[] args) {
+        Jobless jobless = new Jobless(12, 365, 8 * 365f, "Google");
+        System.out.println(jobless.months);
+        System.out.println(jobless.days);
+        System.out.println(jobless.hours);
+    }
+}
+
